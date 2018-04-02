c2c45fb5d9fea5e8a67c1061e46fc2eaf67e55c6
helong <jiaotangxiaodu@163.com>
2018-04-02 20:43:46 +0800

    完成商家入驻和商家审核

--- sellergoods_interface/src/main/java/com/itheima/pinyougou/sellergoods/service/SellerService.java
+++ sellergoods_interface/src/main/java/com/itheima/pinyougou/sellergoods/service/SellerService.java
@@ -1,61 +1,69 @@
 package com.itheima.pinyougou.sellergoods.service;
+
 import com.itheima.pinyougou.entity.PageResult;
 import com.itheima.pinyougou.pojo.TbSeller;
 
 import java.util.List;
+
 /**
  * 服务层接口
- * @author Administrator
  *
+ * @author Administrator
  */
 public interface SellerService {
 
-	/**
-	 * 返回全部列表
-	 * @return
-	 */
-	public List<TbSeller> findAll();
-	
-	
-	/**
-	 * 返回分页列表
-	 * @return
-	 */
-	public PageResult findPage(int pageNum, int pageSize);
-	
-	
-	/**
-	 * 增加
-	*/
-	public void add(TbSeller seller);
-	
-	
-	/**
-	 * 修改
-	 */
-	public void update(TbSeller seller);
-	
+    /**
+     * 返回全部列表
+     *
+     * @return
+     */
+    public List<TbSeller> findAll();
 
-	/**
-	 * 根据ID获取实体
-	 * @param id
-	 * @return
-	 */
-	public TbSeller findOne(String id);
-	
-	
-	/**
-	 * 批量删除
-	 * @param ids
-	 */
-	public void delete(String[] ids);
 
-	/**
-	 * 分页
-	 * @param pageNum 当前页 码
-	 * @param pageSize 每页记录数
-	 * @return
-	 */
-	public PageResult findPage(TbSeller seller, int pageNum, int pageSize);
-	
+    /**
+     * 返回分页列表
+     *
+     * @return
+     */
+    public PageResult findPage(int pageNum, int pageSize);
+
+
+    /**
+     * 增加
+     */
+    public void add(TbSeller seller);
+
+
+    /**
+     * 修改
+     */
+    public void update(TbSeller seller);
+
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    public TbSeller findOne(String id);
+
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     */
+    public void delete(String[] ids);
+
+    /**
+     * 分页
+     *
+     * @param pageNum  当前页 码
+     * @param pageSize 每页记录数
+     * @return
+     */
+    public PageResult findPage(TbSeller seller, int pageNum, int pageSize);
+
+    void updateStatus(String sellerId, String status);
 }
